c683086c9c0ebac5e064f8b3cc32a2320ad8ffb3
Muhammad <developer.mdminhaj@gmail.com>
2022-08-18 19:13:10 +0600

    feat: Improved UI

--- components/list/list-item.jsx
+++ components/list/list-item.jsx
@@ -1,16 +1,16 @@
 export const ListItem = ({ keyName, keyText, disabledDivider }) => {
   if (disabledDivider) {
     return (
-      <li className="flex justify-between p-2 border-none">
-        <span className="font-[500] text-[#53646c]">{keyName}</span>
-        <span>{keyText}</span>
+      <li className="flex justify-between p-2 border-none font-mono">
+        <span className="font-bold pr-3 text-[#53646c]">{keyName}</span>
+        <span className="text-right">{keyText}</span>
       </li>
     )
   }
   return (
-    <li className="flex justify-between p-2 border-b">
-      <span className="font-[500] text-[#53646c]">{keyName}</span>
-      <span>{keyText}</span>
+    <li className="flex justify-between p-2 border-b font-mono">
+      <span className="font-bold text-[#53646c] pr-3">{keyName}</span>
+      <span className="text-right">{keyText}</span>
     </li>
   )
 }
